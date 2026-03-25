0c2d599ee3a7c149c9e27db52676cc6a36a0e656
alexkegley <kegleya9784@my.uwstout.edu>
2026-03-25 13:15:26 -0500

    inputs

--- prim.cpp
+++ prim.cpp
@@ -1,1 +1,27 @@
 //All procedures including main
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+    
+    int Vertices, Edges;
+    std::cin >> Vertices;
+    std::cout << "Enter the number of edges: ";
+    std::cout << Vertices;
+    std::cin >> Edges;
+    int numberOfLinesFromInput;
+    std::string input = "";
+    
+    
+    
+    
+
+
+
+}
+
+void PrimsAlgorithm(int Vertices, int Edges) {
+
+}
